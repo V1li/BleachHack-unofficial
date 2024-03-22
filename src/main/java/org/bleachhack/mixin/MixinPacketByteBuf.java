10a90733179a0015f124d56c3c1f946cb85ada7b
Vili <vili@riseup.net>
2024-03-22 17:28:56 +0200

    Fixed MixinPacketByteBuf

--- src/main/java/org/bleachhack/mixin/MixinPacketByteBuf.java
+++ src/main/java/org/bleachhack/mixin/MixinPacketByteBuf.java
@@ -6,14 +6,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import net.minecraft.nbt.NbtTagSizeTracker;
+import net.minecraft.nbt.NbtSizeTracker;
 import net.minecraft.network.PacketByteBuf;
 
 @Mixin(PacketByteBuf.class)
 public class MixinPacketByteBuf {
 
-	@ModifyArg(method = "readNbt", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/PacketByteBuf;readNbt(Lnet/minecraft/nbt/NbtTagSizeTracker;)Lnet/minecraft/nbt/NbtElement;"))
-    private NbtTagSizeTracker increaseLimit(NbtTagSizeTracker in) {
-        return ModuleManager.getModule(AntiChunkBan.class).isEnabled() ? NbtTagSizeTracker.ofUnlimitedBytes() : in;
+	@ModifyArg(method = "readNbt", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/PacketByteBuf;readNbt(Lnet/minecraft/nbt/NbtSizeTracker;)Lnet/minecraft/nbt/NbtElement;"))
+    private NbtSizeTracker increaseLimit(NbtSizeTracker in) {
+        return ModuleManager.getModule(AntiChunkBan.class).isEnabled() ? NbtSizeTracker.ofUnlimitedBytes() : in;
     }
 }
